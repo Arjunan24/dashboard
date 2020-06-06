98c1d049984eb48517ddf19548891504dad52967
SreeAnanthaKannnan <Sree.kannan@rapidqube.com>
2020-06-06 15:54:41 +0530

    bootom navigator added

--- App.js
+++ App.js
@@ -4,12 +4,18 @@ import React from 'react';
 import Dashboard from "./src/dashboard.js" 
 import TopUp from "./src/topup"
 import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
-import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialIcons';
+import MaterialDashboard from 'react-native-vector-icons/MaterialIcons'
+import MaterialTopUp from 'react-native-vector-icons/AntDesign'
+import MaterialMore from "react-native-vector-icons/Feather"
 
+import { NavigationContainer } from '@react-navigation/native';
 const Tab = createMaterialBottomTabNavigator();
 
 function MyTabs() {
+  
   return (
+    <NavigationContainer>
     <Tab.Navigator
       initialRouteName="Feed"
       activeColor="#e91e63"
@@ -19,33 +25,45 @@ function MyTabs() {
         name="Feed"
         component={Dashboard}
         options={{
-          tabBarLabel: 'Home',
+          tabBarLabel: 'Dashboard',
           tabBarIcon: ({ color }) => (
-            <MaterialCommunityIcons name="home" color={color} size={26} />
+            <MaterialDashboard name="dashboard" color={color} size={30} />
           ),
         }}
       />
+      <Tab.Screen
+        name="topUp"
+        component={TopUp}
+        options={{
+          tabBarLabel: 'Top Up',
+          tabBarIcon: ({ color }) => (
+            <MaterialTopUp name="pluscircleo" color={color} size={26} />
+          ),
+        }}
+      />
+
       <Tab.Screen
         name="Notifications"
         component={TopUp}
         options={{
-          tabBarLabel: 'Updates',
+          tabBarLabel: 'Notification',
           tabBarIcon: ({ color }) => (
-            <MaterialCommunityIcons name="bell" color={color} size={26} />
+            <MaterialCommunityIcons name="notifications" color={color} size={26} />
           ),
         }}
       />
       <Tab.Screen
-        name="Profile"
+        name="more"
         component={TopUp}
         options={{
-          tabBarLabel: 'Profile',
+          tabBarLabel: 'more',
           tabBarIcon: ({ color }) => (
-            <MaterialCommunityIcons name="account" color={color} size={26} />
+            <MaterialMore name="more-horizontal" color={color} size={26} />
           ),
         }}
       />
     </Tab.Navigator>
+    </NavigationContainer>
   );
 }
 
--- src/dashboard.js
+++ src/dashboard.js
@@ -6,16 +6,98 @@ import {
   View,
   Text,
   StatusBar,
+  FlatList,
+  
 } from 'react-native';
 
 
 function Dashboard () {
+
+    const elecData=[
+        {
+        electricityNumber:"ElONO62679",
+        date:"2020/01/07",
+        price:"-R25.3"
+       },
+       {
+        electricityNumber:"ElONO62679",
+        date:"2020/02/07",
+        price:"-R25.3"
+       },
+       {
+        electricityNumber:"ElONO62690",
+        date:"2020/03/07",
+        price:"-R26.3"
+       },
+       {
+        electricityNumber:"ElONO62688",
+        date:"2020/04/07",
+        price:"-R22.3"
+       },
+       {
+        electricityNumber:"ElONO626329",
+        date:"2020/05/07",
+        price:"-R25.9"
+       },
+       {
+        electricityNumber:"ElONO62679",
+        date:"2020/06/07",
+        price:"-R22.3"
+       },
+]
     return(
-      <View>
-        <Text>Dashboard</Text>
+      <View  style={styles.container}>
+         <View>
+         <SafeAreaView >
+      <Text >Electricity</Text>
+      <FlatList
+        data={elecData}
+       
+        renderItem={({ item }) => (
+        <View  style={styles.item}>
+            <Text>{item.electricityNumber}</Text>
+            <Text>{item.date}</Text>
+        </View>
+        )}
+        keyExtractor={item => item.id}
+        extraData={"selected"}
+      />
+    </SafeAreaView>
+         </View>
+         <View >
+         <SafeAreaView >
+      <Text >View Consumptions</Text>
+      <FlatList
+        data={elecData}
+       
+        renderItem={({ item }) => (
+        <View  style={styles.item}>
+            <Text>{item.electricityNumber}</Text>
+            <Text>{item.date}</Text>
+        </View>
+        )}
+        keyExtractor={item => item.id}
+        extraData={"selected"}
+      />
+    </SafeAreaView>
+         </View>
         </View>
     )
+
   }
+
+  const styles = StyleSheet.create({  
+    container: {  
+        flex: 1,  
+        flexWrap:"wrap",
+        flexDirection:"row"
+    },  
+    item: {  
+        padding: 10,  
+        fontSize: 18,  
+        height: 44,  
+    },  
+})  
   export default Dashboard;
 
   
